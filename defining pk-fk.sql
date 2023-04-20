5853ec32327382696c0cc70a07ca88334800c07b
Ekraj Pokhrel <ekraj.pok@gmail.com>
2023-04-20 20:31:27 +0200

    added course_name, course_id defined and primary key and foreign key defined

---
+++ defining pk-fk.sql
@@ -0,0 +1,36 @@
+ALTER TABLE project.comments
+ADD PRIMARY KEY (comment_id);
+
+ALTER TABLE project.course_name
+ADD PRIMARY KEY (course_id);	
+
+ALTER TABLE project.badge_id
+ADD PRIMARY KEY (badge_id);
+
+ALTER TABLE project.locations
+ADD PRIMARY KEY (location_id);
+
+ALTER TABLE project.schools
+ADD PRIMARY KEY (school_id);
+
+ALTER TABLE project.badges
+ADD CONSTRAINT school_id FOREIGN KEY (school_id) REFERENCES schools(school_id),
+ADD CONSTRAINT badge_id FOREIGN KEY (badge_id) REFERENCES badge_id(badge_id);
+
+ALTER TABLE comments
+ADD CONSTRAINT FOREIGN KEY (school_id) REFERENCES schools(school_id);
+
+ALTER TABLE courses
+ADD CONSTRAINT FOREIGN KEY (school_id) REFERENCES schools(school_id),
+ADD CONSTRAINT FOREIGN KEY (school_id) REFERENCES schools(school_id);
+
+ALTER TABLE locations
+ADD CONSTRAINT FOREIGN KEY (school_id) REFERENCES schools(school_id);
+
+ALTER TABLE courses
+ADD CONSTRAINT FOREIGN KEY (school_id) REFERENCES schools(school_id);
+
+
+
+
+
